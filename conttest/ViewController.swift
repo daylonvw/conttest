40a4d36d02a5c5509cc7ab57cf9022720ceaad32
d.r. van wel <daylonvanwel@mbp-van-dr.cs.ad.klmcorp.net>
2017-03-14 11:07:46 +0100

    Initial Commit

---
+++ conttest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  conttest
+//
+//  Created by d.r. van wel on 14-03-17.
+//  Copyright © 2017 d.r. van wel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
